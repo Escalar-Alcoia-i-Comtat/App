0696a7eb65dab9dcf7fcf03ec6ff3c8a52c4618d
Arnau Mora <arnyminerz@proton.me>
2023-11-13 13:51:30 +0100

    Progress on image loading
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/cache/ImageCache.kt
+++ composeApp/src/commonMain/kotlin/cache/ImageCache.kt
@@ -1,5 +1,13 @@
 package cache
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import cache.Files.delete
 import cache.Files.exists
 import cache.Files.mkdirs
@@ -10,52 +18,70 @@ import io.ktor.client.HttpClient
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsChannel
 import io.ktor.util.toByteArray
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.channelFlow
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
+import kotlinx.coroutines.launch
 import network.Backend
 
 object ImageCache {
     private val client = HttpClient()
 
-    fun get(uuid: String): Flow<ByteArray> {
-        if (!storageProvider.cacheDirectory.exists()) {
-            require(storageProvider.cacheDirectory.mkdirs()) {
-                "Could not create cache directory (${storageProvider.cacheDirectory})."
+    @Composable
+    fun collectStateOf(uuid: String): State<ByteArray?> {
+        val state: MutableState<ByteArray?> = remember { mutableStateOf(null) }
+
+        /**
+         * Stores whether the image data has already been fetched from the server in this lifecycle.
+         */
+        var alreadyFetchedUpdate by remember { mutableStateOf(false) }
+
+        LaunchedEffect(uuid) {
+            if (!storageProvider.cacheDirectory.exists()) {
+                require(storageProvider.cacheDirectory.mkdirs()) {
+                    "Could not create cache directory (${storageProvider.cacheDirectory})."
+                }
+            }
+
+            val file = storageProvider.cacheDirectory + uuid
+            val hashFile = storageProvider.cacheDirectory + uuid + "_hash"
+
+            Napier.v(tag = "ImageCache-$uuid") { "$file" }
+
+            CoroutineScope(Dispatchers.IO).launch {
+                if (file.exists()) {
+                    Napier.d(tag = "ImageCache-$uuid") { "Already cached, sending bytes..." }
+                    state.value = file.readAllBytes()
+                }
+
+                if (!file.exists() || !alreadyFetchedUpdate) launch(Dispatchers.IO) {
+                    try {
+                        Napier.v(tag = "ImageCache-$uuid") { "Requesting file data..." }
+                        val fileRequest = Backend.requestFile(uuid)
+                        if (!hashFile.exists() || hashFile.readAllBytes().decodeToString() != fileRequest.hash) {
+                            // Download the file again
+                            if (file.exists()) file.delete()
+                            if (hashFile.exists()) hashFile.delete()
+
+                            val bytes = client.get(fileRequest.download).bodyAsChannel().toByteArray()
+                            file.write(bytes)
+                            hashFile.write(fileRequest.hash.encodeToByteArray())
+
+                            state.value = bytes
+                        } else {
+                            Napier.v(tag = "ImageCache-$uuid") { "Cached file up to date" }
+                        }
+
+                        alreadyFetchedUpdate = true
+                    } catch (e: Exception) {
+                        Napier.w(throwable = e, tag = "ImageCache-$uuid") {
+                            "Could not get file metadata."
+                        }
+                    }
+                }
             }
         }
 
-        val file = storageProvider.cacheDirectory + uuid
-        val hashFile = storageProvider.cacheDirectory + uuid + "_hash"
-
-        Napier.v(tag = "ImageCache-$uuid") { "$file" }
-
-        return channelFlow {
-            if (file.exists()) {
-                Napier.d(tag = "ImageCache-$uuid") { "Already cached, sending bytes..." }
-                send(file.readAllBytes())
-            }
-
-            try {
-                Napier.v(tag = "ImageCache-$uuid") { "Requesting file data..." }
-                val fileRequest = Backend.requestFile(uuid)
-                if (!hashFile.exists() || hashFile.readAllBytes().decodeToString() != fileRequest.hash) {
-                    // Download the file again
-                    if (file.exists()) file.delete()
-                    if (hashFile.exists()) hashFile.delete()
-
-                    val bytes = client.get(fileRequest.download).bodyAsChannel().toByteArray()
-                    file.write(bytes)
-                    hashFile.write(fileRequest.hash.encodeToByteArray())
-
-                    send(bytes)
-                } else {
-                    Napier.v(tag = "ImageCache-$uuid") { "Cached file up to date" }
-                }
-            } catch (e: Exception) {
-                Napier.w(throwable = e, tag = "ImageCache-$uuid") {
-                    "Could not get file metadata."
-                }
-            }
-        }
+        return state
     }
 }
--- composeApp/src/commonMain/kotlin/ui/list/DataCard.kt
+++ composeApp/src/commonMain/kotlin/ui/list/DataCard.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -49,7 +48,7 @@ fun <T: DataTypeWithImage> DataCard(
             fontSize = 20.sp
         )
 
-        val image by ImageCache.get(item.image).collectAsState(null)
+        val image by ImageCache.collectStateOf(item.image)
 
         Box(
             modifier = Modifier
