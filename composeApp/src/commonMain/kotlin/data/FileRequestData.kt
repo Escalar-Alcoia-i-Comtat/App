afa76dd30973253a9b5fd031f968330d2f679d34
Arnau Mora <arnyminerz@proton.me>
2023-11-17 12:55:36 +0100

    Added UUID
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/data/FileRequestData.kt
+++ composeApp/src/commonMain/kotlin/data/FileRequestData.kt
@@ -4,6 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class FileRequestData(
+    val uuid: String,
     val hash: String,
     val filename: String,
     val download: String,
