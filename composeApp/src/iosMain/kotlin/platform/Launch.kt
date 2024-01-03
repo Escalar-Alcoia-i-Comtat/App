c548326091f2c3b6aba9572e54d8a44649b88df2
Arnau Mora <arnyminerz@proton.me>
2024-01-03 15:57:35 +0100

    Implemented URL launching
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/iosMain/kotlin/platform/Launch.kt
+++ composeApp/src/iosMain/kotlin/platform/Launch.kt
@@ -1,7 +1,10 @@
 package platform
 
 import data.generic.LatLng
+import platform.Foundation.NSURL
+import platform.UIKit.UIApplication
 
+private const val DEFAULT_ZOOM = 6
 
 /**
  * Shows the given [point] in an external application.
@@ -14,5 +17,18 @@ import data.generic.LatLng
  */
 actual fun launchPoint(point: LatLng, label: String?): Boolean {
     // see: https://developers.google.com/maps/documentation/urls/ios-urlscheme
-    TODO("Launch URL in iOS")
+
+    // Check whether Google Maps is installed
+    if (UIApplication.sharedApplication.canOpenURL(NSURL("comgooglemaps://"))){
+        // Google Maps is available, launch in the app
+        UIApplication.sharedApplication.openURL(
+            NSURL("comgooglemaps://?center=${point.longitude},${point.latitude}&zoom=$DEFAULT_ZOOM")
+        )
+    } else {
+        // Google Maps is not installed, launch in the browser
+        UIApplication.sharedApplication.openURL(
+            NSURL("https://www.google.com/maps/@${point.longitude},${point.latitude},${DEFAULT_ZOOM}z")
+        )
+    }
+    return true
 }
