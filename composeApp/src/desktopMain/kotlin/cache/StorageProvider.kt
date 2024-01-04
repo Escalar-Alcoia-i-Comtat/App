aded7ffa53c8a98dc1acc039a86798fc9253ba90
Arnau Mora <arnyminerz@proton.me>
2024-01-04 09:15:00 +0100

    Typo
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/desktopMain/kotlin/cache/StorageProvider.kt
+++ composeApp/src/desktopMain/kotlin/cache/StorageProvider.kt
@@ -3,5 +3,7 @@ package cache
 actual class StorageProvider {
     private val home = java.io.File(System.getProperty("user.home"))
 
-    actual val cacheDirectory: File = java.io.File(home, ".escalaralcoiaicomtat").file
+    actual val cacheDirectory: File = java.io.File(home, ".escalaralcoiaicomtat").let {
+        File(it.absolutePath)
+    }
 }
