b335560a5d9079e7c40480ca63557fa55c60e00c
Arnau Mora <arnyminerz@proton.me>
2024-01-10 21:43:21 +0100

    Added back handler for iOS
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -3,13 +3,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.CurrentScreen
 import cafe.adriel.voyager.navigator.Navigator
 import data.EDataType
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 import network.connectivityStatus
 import ui.screen.AppScreen
+import utils.createStore
+
+val store = CoroutineScope(SupervisorJob()).createStore()
 
 @Composable
 fun App(
