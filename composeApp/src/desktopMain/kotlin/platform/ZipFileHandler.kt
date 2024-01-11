a88ea7b957bdca243142614ad079f496a3be8dd7
Arnau Mora <arnyminerz@proton.me>
2024-01-11 07:50:12 -0800

    Added support for maps on desktop (#11)
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/desktopMain/kotlin/platform/ZipFileHandler.kt
+++ composeApp/src/desktopMain/kotlin/platform/ZipFileHandler.kt
@@ -12,17 +12,26 @@ actual object ZipFileHandler {
      * Extracts the zip file stored at [file] into the given directory ([dir]).
      */
     actual suspend fun unzip(file: File, dir: File) {
+        if (!dir.exists()) dir.mkdirs()
         Napier.d { "Extracting $file into $dir..." }
         val path = file.path.toPath()
         val zipFileSystem = fileSystem.openZip(path)
         for (zipPath in zipFileSystem.listRecursively("/".toPath())) {
             val target = File(dir, zipPath.toString())
+            if (target.exists()) continue
+            if (zipFileSystem.metadata(zipPath).isDirectory) {
+                zipFileSystem.createDirectories(zipPath)
+                continue
+            }
+
+            Napier.v { "  Reading #ZIP~$zipPath..." }
             val contents = zipFileSystem.source(zipPath).use { source ->
                 source.buffer().use { buffer ->
                     buffer.readByteArray()
                 }
             }
-            Napier.v { " Writing #ZIP/$zipPath into $target..." }
+            Napier.v { "  Writing #ZIP/$zipPath into $target..." }
+            target.parent.mkdirs()
             target.write(contents)
         }
         Napier.d { "Extraction complete!" }
