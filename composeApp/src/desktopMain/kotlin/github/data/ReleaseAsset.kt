db280e72c50d4584c7a758c536a50d3891d5c166
Arnau Mora <arnyminerz@proton.me>
2024-01-12 23:49:03 +0100

    Simplified model
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/desktopMain/kotlin/github/data/ReleaseAsset.kt
+++ composeApp/src/desktopMain/kotlin/github/data/ReleaseAsset.kt
@@ -7,11 +7,7 @@ import kotlinx.serialization.Serializable
 data class ReleaseAsset(
     val url: String,
     @SerialName("browser_download_url") val browserDownloadUrl: String,
-    val id: Long,
-    @SerialName("node_id") val nodeId: String,
     val name: String,
-    val label: String,
-    val state: String,
     @SerialName("content_type") val contentType: String,
     val size: Long
 )
