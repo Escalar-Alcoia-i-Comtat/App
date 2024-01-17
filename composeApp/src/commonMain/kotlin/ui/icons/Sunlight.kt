625fe77d32fabd89b249c84c89cebbfa2292b36d
Arnau Mora Gras <arnyminerz@proton.me>
2024-01-17 13:41:59 +0100

    Added icons
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

---
+++ composeApp/src/commonMain/kotlin/ui/icons/Sunlight.kt
@@ -0,0 +1,94 @@
+package ui.icons
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.materialPath
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+
+val Icons.Rounded.Sunlight: ImageVector
+    get() {
+        if (_sunlight != null) {
+            return _sunlight!!
+        }
+        _sunlight = ImageVector.Builder(
+            name = "Rounded.Sunlight",
+            defaultWidth = 24.0.dp,
+            defaultHeight = 24.0.dp,
+            viewportWidth = 24.0F,
+            viewportHeight = 24.0F,
+        ).materialPath {
+            verticalLineToRelative(0.0F)
+            moveTo(11.984375F, -0.013671875F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 11.0F, 1.0F)
+            lineTo(11.0F, 3.0F)
+            arcTo(1.0001F, 1.0001F, 0.0F, true, false, 13.0F, 3.0F)
+            lineTo(13.0F, 1.0F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 11.984375F, -0.013671875F)
+
+            moveTo(2.9902344F, 1.9902344F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 2.2929688F, 3.7070312F)
+            lineTo(6.408203F, 7.8222656F)
+            curveTo(5.530809F, 8.989132F, 5.0F, 10.430148F, 5.0F, 12.0F)
+            curveTo(5.0F, 15.859F, 8.141F, 19.0F, 12.0F, 19.0F)
+            curveTo(13.569852F, 19.0F, 15.010868F, 18.46919F, 16.177734F, 17.591797F)
+            lineTo(20.292969F, 21.707031F)
+            arcTo(1.0001F, 1.0001F, 0.0F, true, false, 21.707031F, 20.292969F)
+            lineTo(17.597656F, 16.183594F)
+            curveTo(17.601814F, 16.178043F, 17.607185F, 16.17353F, 17.611328F, 16.167969F)
+            lineTo(16.177734F, 14.732422F)
+            curveTo(16.177734F, 14.734422F, 16.174828F, 14.736281F, 16.173828F, 14.738281F)
+            lineTo(9.261719F, 7.826172F)
+            curveTo(9.263719F, 7.825172F, 9.265578F, 7.8232656F, 9.267578F, 7.8222656F)
+            lineTo(7.8320312F, 6.388672F)
+            curveTo(7.826469F, 6.3928137F, 7.821956F, 6.398186F, 7.8164062F, 6.4023438F)
+            lineTo(3.7070312F, 2.2929688F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 2.9902344F, 1.9902344F)
+
+            moveTo(20.464844F, 2.5058594F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 19.777344F, 2.8085938F)
+            lineTo(18.292969F, 4.2929688F)
+            arcTo(1.0001F, 1.0001F, 0.0F, true, false, 19.707031F, 5.7070312F)
+            lineTo(21.191406F, 4.2226562F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 20.464844F, 2.5058594F)
+
+            moveTo(12.0F, 5.0F)
+            curveTo(11.185F, 5.0F, 10.406734F, 5.146297F, 9.677734F, 5.404297F)
+            lineTo(10.476562F, 6.203125F)
+            curveTo(10.473562F, 6.204125F, 10.47175F, 6.204078F, 10.46875F, 6.205078F)
+            lineTo(17.794922F, 13.533203F)
+            curveTo(17.795921F, 13.530203F, 17.795876F, 13.528391F, 17.796875F, 13.525391F)
+            lineTo(18.595703F, 14.324219F)
+            curveTo(18.852703F, 13.594219F, 19.0F, 12.815F, 19.0F, 12.0F)
+            curveTo(19.0F, 8.141F, 15.859F, 5.0F, 12.0F, 5.0F)
+
+            moveTo(1.0F, 11.0F)
+            arcTo(1.0001F, 1.0001F, 0.0F, true, false, 1.0F, 13.0F)
+            lineTo(3.0F, 13.0F)
+            arcTo(1.0001F, 1.0001F, 0.0F, true, false, 3.0F, 11.0F)
+            lineTo(1.0F, 11.0F)
+
+            moveTo(21.0F, 11.0F)
+            arcTo(1.0001F, 1.0001F, 0.0F, true, false, 21.0F, 13.0F)
+            lineTo(23.0F, 13.0F)
+            arcTo(1.0001F, 1.0001F, 0.0F, true, false, 23.0F, 11.0F)
+            lineTo(21.0F, 11.0F)
+
+            moveTo(4.9804688F, 17.990234F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 4.2929688F, 18.292969F)
+            lineTo(2.8085938F, 19.777344F)
+            arcTo(1.0001F, 1.0001F, 0.0F, true, false, 4.2226562F, 21.191406F)
+            lineTo(5.7070312F, 19.707031F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 4.9804688F, 17.990234F)
+
+            moveTo(11.984375F, 19.986328F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 11.0F, 21.0F)
+            lineTo(11.0F, 23.0F)
+            arcTo(1.0001F, 1.0001F, 0.0F, true, false, 13.0F, 23.0F)
+            lineTo(13.0F, 21.0F)
+            arcTo(1.0001F, 1.0001F, 0.0F, false, false, 11.984375F, 19.986328F)
+
+            close()
+        }.build()
+        return _sunlight!!
+    }
+private var _sunlight: ImageVector? = null
