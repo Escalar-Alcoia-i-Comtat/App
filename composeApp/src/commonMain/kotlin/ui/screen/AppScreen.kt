b12f0d76569ac9e8510e1c676b7006e39f5d4432
Arnau Mora <arnyminerz@proton.me>
2024-03-26 16:15:28 +0100

    Tooltip migration
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- composeApp/src/commonMain/kotlin/ui/screen/AppScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/screen/AppScreen.kt
@@ -29,9 +29,12 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
-import androidx.compose.material3.PlainTooltipBox
+import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -199,9 +202,13 @@ class AppScreen(
                                     AnimatedVisibility(
                                         visible = !isNetworkConnected
                                     ) {
-                                        PlainTooltipBox(
+                                        TooltipBox(
+                                            positionProvider = TooltipDefaults.rememberPlainTooltipPositionProvider(),
+                                            state = rememberTooltipState(),
                                             tooltip = {
-                                                Text(stringResource(MR.strings.status_network_unavailable))
+                                                PlainTooltip {
+                                                    Text(stringResource(MR.strings.status_network_unavailable))
+                                                }
                                             }
                                         ) {
                                             IconButton(
