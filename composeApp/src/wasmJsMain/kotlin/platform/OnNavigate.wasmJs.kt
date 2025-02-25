81912376f74121e4c6f2f12c5b7dfc34eab5c52a
Arnau Mora <arnyminerz@proton.me>
2025-02-25 18:09:52 +0100

    Disabled history pushing
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/wasmJsMain/kotlin/platform/OnNavigate.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/platform/OnNavigate.wasmJs.kt
@@ -1,12 +1,14 @@
 package platform
 
-import kotlinx.browser.window
 import ui.navigation.Destination
 
+// TODO: Enable history pushing when a solution is found for relative URL loading
+// String resources are sometimes loaded asynchronously, so they are not found on lower paths
+
 actual fun <T : Destination> onNavigate(destination: T) {
-    window.history.pushState(destination.name.toJsString(), destination.name, destination.path)
+    // window.history.pushState(destination.name.toJsString(), destination.name, destination.path)
 }
 
 actual fun <T : Destination> initialDestination(destination: T) {
-    window.history.replaceState(destination.name.toJsString(), destination.name, destination.path)
+    // window.history.replaceState(destination.name.toJsString(), destination.name, destination.path)
 }
