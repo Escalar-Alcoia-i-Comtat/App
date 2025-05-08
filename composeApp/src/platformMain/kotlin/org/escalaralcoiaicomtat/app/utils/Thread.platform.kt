5f0b291fb9e11de8b57f01bc22d71002118bc696
Arnau Mora <arnyminerz@proton.me>
2025-05-08 20:53:40 +0200

    Simplified threading
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/platformMain/kotlin/org/escalaralcoiaicomtat/app/utils/Thread.platform.kt
+++ composeApp/src/platformMain/kotlin/org/escalaralcoiaicomtat/app/utils/Thread.platform.kt
@@ -2,6 +2,7 @@ package org.escalaralcoiaicomtat.app.utils
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 
 actual val Dispatchers.IO: CoroutineDispatcher
     get() = Dispatchers.IO
