70228b9f8a95db84533ddd3a545f7cea4d46f124
Arnau Mora <arnyminerz@proton.me>
2025-05-13 13:18:51 +0200

    Implemented scheduled sync for desktop
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/desktopMain/kotlin/org/escalaralcoiaicomtat/app/sync/SyncManager.kt
+++ composeApp/src/desktopMain/kotlin/org/escalaralcoiaicomtat/app/sync/SyncManager.kt
@@ -1,17 +1,42 @@
 package org.escalaralcoiaicomtat.app.sync
 
+import io.github.aakira.napier.Napier
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 import org.escalaralcoiaicomtat.app.data.DataTypes
-import javax.naming.OperationNotSupportedException
+import org.escalaralcoiaicomtat.app.database.SettingsKeys
+import org.escalaralcoiaicomtat.app.database.settings
+import org.escalaralcoiaicomtat.app.sync.SyncProcess.Cause
 
 actual object SyncManager {
-    actual fun schedule() {
-        throw OperationNotSupportedException("Desktop doesn't support scheduling periodic works.")
+    private suspend fun runIfSchedulePermits(
+        settingsKey: String,
+        process: SyncProcess,
+    ) {
+        val lastSync = settings
+            .getLongOrNull(settingsKey)
+            ?.let { Instant.fromEpochMilliseconds(it) }
+        val now = Clock.System.now()
+
+        // Synchronize if never synced, or every 12 hours
+        if (lastSync == null || (now - lastSync).inWholeHours > DataSync.SYNC_PERIOD_HOURS) {
+            process.start(mapOf(SyncProcess.ARG_TYPE to Cause.Scheduled))
+        } else {
+            Napier.d { "Won't run synchronization ($settingsKey). Last run: ${(now - lastSync).inWholeHours} hours ago" }
+        }
     }
 
-    actual fun run(cause: SyncProcess.Cause, syncId: Pair<DataTypes<*>, Int>?) {
+    actual fun schedule() {
+        CoroutineScope(Dispatchers.IO).launch {
+            runIfSchedulePermits(SettingsKeys.LAST_SYNC_TIME, DataSync)
+            runIfSchedulePermits(SettingsKeys.LAST_BLOCK_SYNC_TIME, BlockingSync)
+        }
+    }
+
+    actual fun run(cause: Cause, syncId: Pair<DataTypes<*>, Int>?) {
         CoroutineScope(Dispatchers.IO).launch {
             DataSync.start(cause, syncId)
 
