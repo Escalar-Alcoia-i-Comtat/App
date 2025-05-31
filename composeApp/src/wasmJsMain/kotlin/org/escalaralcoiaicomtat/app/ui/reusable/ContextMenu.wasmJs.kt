71e69f373e03b8c8a24237845d13e690945f48cc
Arnau Mora <arnyminerz@proton.me>
2025-05-31 12:55:31 +0200

    Use proper desktop context menu
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/wasmJsMain/kotlin/org/escalaralcoiaicomtat/app/ui/reusable/ContextMenu.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/org/escalaralcoiaicomtat/app/ui/reusable/ContextMenu.wasmJs.kt
@@ -3,9 +3,10 @@ package org.escalaralcoiaicomtat.app.ui.reusable
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -18,7 +19,7 @@ import androidx.compose.ui.Modifier
 actual fun ContextMenu(
     modifier: Modifier,
     enabled: Boolean,
-    dropdownContent: @Composable ColumnScope.() -> Unit,
+    items: List<ContextMenuItem>,
     content: @Composable () -> Unit
 ) {
     var expanded by remember { mutableStateOf(false) }
@@ -43,6 +44,13 @@ actual fun ContextMenu(
         ExposedDropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false }
-        ) { dropdownContent() }
+        ) {
+            items.map { item ->
+                DropdownMenuItem(
+                    text = { Text(item.label()) },
+                    onClick = item.onClick,
+                )
+            }
+        }
     }
 }
