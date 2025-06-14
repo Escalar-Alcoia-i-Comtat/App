12a7e30b31e0ad105652227ae66f4ff58afddbcc
Arnau Mora <arnyminerz@proton.me>
2025-06-14 17:59:39 +0200

    Set default distance unit based on system preference
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/androidMain/kotlin/org/escalaralcoiaicomtat/android/App.kt
+++ composeApp/src/androidMain/kotlin/org/escalaralcoiaicomtat/android/App.kt
@@ -1,14 +1,22 @@
 package org.escalaralcoiaicomtat.android
 
 import android.app.Application
+import android.icu.util.LocaleData
+import android.icu.util.LocaleData.MeasurementSystem
+import android.icu.util.ULocale
+import android.os.Build
+import com.russhwolf.settings.set
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import org.escalaralcoiaicomtat.app.cache.StorageProvider
 import org.escalaralcoiaicomtat.app.cache.storageProvider
+import org.escalaralcoiaicomtat.app.database.SettingsKeys
 import org.escalaralcoiaicomtat.app.database.getDatabaseBuilder
 import org.escalaralcoiaicomtat.app.database.roomDatabaseBuilder
+import org.escalaralcoiaicomtat.app.database.settings
 import org.escalaralcoiaicomtat.app.initializeSentry
 import org.escalaralcoiaicomtat.app.sync.SyncManager
+import org.escalaralcoiaicomtat.app.utils.unit.DistanceUnits
 
 class App : Application() {
     override fun onCreate() {
@@ -30,5 +38,20 @@ class App : Application() {
 
         // Schedule the sync workers
         SyncManager.scheduleWorker()
+
+        // If API level is greater or equal than P
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // Set the default distance unit if none is set
+            if (!settings.hasKey(SettingsKeys.DISTANCE_UNITS)) {
+                val measurementSystem = LocaleData.getMeasurementSystem(ULocale.getDefault())
+                val system = when (measurementSystem) {
+                    MeasurementSystem.SI -> DistanceUnits.METER
+                    MeasurementSystem.US,  MeasurementSystem.UK -> DistanceUnits.FEET
+                    else -> DistanceUnits.METER
+                }
+                Napier.i { "Setting distance units to $system as per system preference." }
+                settings[SettingsKeys.DISTANCE_UNITS] = system.name
+            }
+        }
     }
 }
