26a7ec056e187c0a92ddf9361914bea82e038f66
Arnau Mora <arnyminerz@proton.me>
2025-06-28 12:32:23 +0200

    Added map exception handler
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- composeApp/src/iosMain/kotlin/org/escalaralcoiaicomtat/app/ui/platform/MapComposable.ios.kt
+++ composeApp/src/iosMain/kotlin/org/escalaralcoiaicomtat/app/ui/platform/MapComposable.ios.kt
@@ -1,20 +1,34 @@
 package org.escalaralcoiaicomtat.app.ui.platform
 
 import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalClipboard
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.role
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.viewinterop.UIKitInteropProperties
 import androidx.compose.ui.viewinterop.UIKitView
+import escalaralcoiaicomtat.composeapp.generated.resources.*
 import io.github.aakira.napier.Napier
 import kotlinx.cinterop.ExperimentalForeignApi
 import org.escalaralcoiaicomtat.app.data.generic.LatLng
@@ -22,6 +36,7 @@ import org.escalaralcoiaicomtat.app.map.placemark.Placemark
 import org.escalaralcoiaicomtat.app.map.style.Style
 import org.escalaralcoiaicomtat.app.map.utils.coordinateRegionOf
 import org.escalaralcoiaicomtat.app.ui.reusable.CircularProgressIndicatorBox
+import org.jetbrains.compose.resources.stringResource
 import platform.MapKit.MKAnnotationProtocol
 import platform.MapKit.MKMapTypeSatellite
 import platform.MapKit.MKMapView
@@ -42,6 +57,9 @@ actual fun MapComposable(
     onMapClick: (() -> Unit)?,
 ) {
     var mapData by remember { mutableStateOf<MapData?>(null) }
+    val error by viewModel.error.collectAsState()
+
+    val clipboard = LocalClipboard.current
 
     LaunchedEffect(kmz) {
         if (kmz != null && mapData == null) viewModel.loadKMZ(kmz) { mapData = it }
@@ -49,7 +67,39 @@ actual fun MapComposable(
 
     OutlinedCard(modifier) {
         AnimatedContent(mapData) { data ->
-            if (data != null) {
+            if (error != null) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background( MaterialTheme.colorScheme.errorContainer)
+                        .clickable {
+                            viewModel.copyException(clipboard)
+                        },
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            Icons.Default.Map,
+                            stringResource(Res.string.error_map_title),
+                            tint = MaterialTheme.colorScheme.onErrorContainer,
+                        )
+                        Text(
+                            text = stringResource(Res.string.error_map_title),
+                            style = MaterialTheme.typography.labelLarge,
+                            color = MaterialTheme.colorScheme.onErrorContainer,
+                            textAlign = TextAlign.Center,
+                        )
+                        Text(
+                            text = stringResource(Res.string.error_map_title),
+                            style = MaterialTheme.typography.labelMedium,
+                            color = MaterialTheme.colorScheme.onErrorContainer,
+                            textAlign = TextAlign.Center,
+                        )
+                    }
+                }
+            } else if (data != null) {
                 val mapView = remember {
                     MKMapView().apply {
                         mapType = MKMapTypeSatellite
--- composeApp/src/iosMain/kotlin/org/escalaralcoiaicomtat/app/ui/platform/MapViewModel.ios.kt
+++ composeApp/src/iosMain/kotlin/org/escalaralcoiaicomtat/app/ui/platform/MapViewModel.ios.kt
@@ -1,7 +1,13 @@
 package org.escalaralcoiaicomtat.app.ui.platform
 
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.platform.ClipEntry
+import androidx.compose.ui.platform.Clipboard
 import com.fleeksoft.ksoup.Ksoup
 import io.github.aakira.napier.Napier
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import org.escalaralcoiaicomtat.app.map.placemark.Placemark
 import org.escalaralcoiaicomtat.app.map.style.Style
 import org.escalaralcoiaicomtat.app.maps.KMZHandler
@@ -9,26 +15,46 @@ import org.escalaralcoiaicomtat.app.ui.model.ViewModelBase
 import kotlin.uuid.Uuid
 
 actual class MapViewModel actual constructor() : ViewModelBase() {
+
+    private val _error = MutableStateFlow<Throwable?>(null)
+    val error: StateFlow<Throwable?> get() = _error.asStateFlow()
+
     fun loadKMZ(
         kmz: Uuid,
         onDocumentLoaded: (data: MapData) -> Unit
     ) {
         launch {
-            val kmlFile = KMZHandler.load(kmz, replaceImagePaths = false)
-            val kmlString = kmlFile.read("doc.kml")!!.decodeToString()
-            val xml = Ksoup.parse(kmlString)
-            val document = xml.root().getElementsByTag("Document")[0]
-            val styles = document.getElementsByTag("Style")
-                .also { Napier.d { "Processing ${it.size} style elements..." } }
-                .mapNotNull(Style.Companion::parse)
-            val placemarks = document.getElementsByTag("Folder")
-                .also { Napier.d { "Processing ${it.size} folders..." } }
-                .flatMap { folder ->
-                    folder.getElementsByTag("Placemark").map { Placemark.parse(it) }
-                }
-                .filterNotNull()
-            onDocumentLoaded(
-                MapData(placemarks, styles)
+            _error.emit(null)
+            try {
+                val kmlFile = KMZHandler.load(kmz, replaceImagePaths = false)
+                val kmlString = kmlFile.read("doc.kml")!!.decodeToString()
+                val xml = Ksoup.parse(kmlString)
+                val document = xml.root().getElementsByTag("Document")[0]
+                val styles = document.getElementsByTag("Style")
+                    .also { Napier.d { "Processing ${it.size} style elements..." } }
+                    .mapNotNull(Style.Companion::parse)
+                val placemarks = document.getElementsByTag("Folder")
+                    .also { Napier.d { "Processing ${it.size} folders..." } }
+                    .flatMap { folder ->
+                        folder.getElementsByTag("Placemark").map { Placemark.parse(it) }
+                    }
+                    .filterNotNull()
+                onDocumentLoaded(
+                    MapData(placemarks, styles)
+                )
+            } catch (e: Exception) {
+                Napier.e(e) { "Could not load map." }
+                _error.tryEmit(e)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalComposeUiApi::class)
+    fun copyException(clipboard: Clipboard) {
+        val exception = _error.value ?: return
+        launch {
+            clipboard.setClipEntry(
+                ClipEntry.withPlainText(exception.toString())
             )
         }
     }
