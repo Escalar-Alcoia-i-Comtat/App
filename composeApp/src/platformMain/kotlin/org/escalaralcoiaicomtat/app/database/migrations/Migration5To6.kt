0cfa32741663ea4d830295fa952a625a6bf219f9
Arnau Mora <arnyminerz@proton.me>
2025-08-28 14:31:55 +0200

    Default null

--- composeApp/src/platformMain/kotlin/org/escalaralcoiaicomtat/app/database/migrations/Migration5To6.kt
+++ composeApp/src/platformMain/kotlin/org/escalaralcoiaicomtat/app/database/migrations/Migration5To6.kt
@@ -6,6 +6,6 @@ import androidx.sqlite.execSQL
 
 object Migration5To6 : Migration(5, 6) {
     override fun migrate(connection: SQLiteConnection) {
-        connection.execSQL("ALTER TABLE SectorEntity ADD COLUMN phoneSignalAvailability TEXT")
+        connection.execSQL("ALTER TABLE SectorEntity ADD COLUMN phoneSignalAvailability TEXT DEFAULT NULL")
     }
 }
